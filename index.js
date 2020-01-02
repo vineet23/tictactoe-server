be8cb8c998b8fca9f062b381bf6306a6c9f8da06
vineet23 <vineet23@users.noreply.github.com>
2020-01-03 02:40:30 +0530

    added rooms

--- index.js
+++ index.js
@@ -15,4 +15,26 @@ var io = socket(server);
 //on connection of a socket
 io.on("connection", function(socket) {
   console.log("made socket connection");
+
+  //on socket disconnected
+  socket.on("disconnect", function() {
+    console.log("user discconected");
+  });
+
+  //on room request ,data is room id
+  socket.on("room", function(data) {
+    console.log("room with id " + data);
+    //joinned the room
+    socket.join(data);
+  });
+
+  //to join a room ,data is room id
+  //used when scanned QR code
+  socket.on("join", function(data) {
+    console.log("joinned room with id " + data);
+    //to join the room first
+    socket.join(data);
+    //to notify the room about the joinning of new player
+    io.to(data).emit("joinned");
+  });
 });
